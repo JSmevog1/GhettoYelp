67cf2c85cfe8e962e213a0627da1655a87c14422
Nguyen <yunguyen@csumb.edu>
2024-12-02 20:30:34 -0800

    RestaurantRepository.java: - Setup and added variables, methods - Add Javadoc ReviewsRepository.java: - Setup and added variables, methods - Add Javadoc
    Waiting for entity classes (restaurant, user, review) to be created

---
+++ app/src/main/java/com/example/ghettoyelp/Database/RestaurantRepository.java
@@ -0,0 +1,37 @@
+package com.example.ghettoyelp.Database;
+
+import android.app.Application;
+
+import com.example.ghettoyelp.Database.DAOs.RestaurantDAO;
+
+import java.util.ArrayList;
+
+/**
+ * @author Yui Nguyen
+ * Last Update: Dec 2nd, 2024
+ * Description:
+ *      A repository to read and write data to RestaurantDAO
+ */
+
+public class RestaurantRepository {
+    // VARIABLES for repository
+    private static RestaurantRepository repository;
+
+    // VARIABLES for DAO
+    // TODO: wait for entity class - Restaurant to be created
+    private final RestaurantDAO restaurantDAO;
+    private ArrayList<Restaurant> allUsers;
+
+    // CONSTRUCTOR
+    public RestaurantRepository(Application application){
+        MainDatabase database = MainDatabase.getDatabase(application);
+        restaurantDAO = database.restaurantDAO();
+    }
+
+    // METHODS to READ and WRITE to DAO
+    // TODO: add method to retrieve all restaurants from DAO
+
+    // TODO: add method to insert an restaurant to DAO
+
+    // TODO: wait for other issues to add other methods
+}
