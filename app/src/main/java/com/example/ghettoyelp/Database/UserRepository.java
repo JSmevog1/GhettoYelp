0791d6065a3b1f3b9a56d369d84d383d6e6584f9
Nguyen <yunguyen@csumb.edu>
2024-12-05 11:38:29 -0800

    UserRepository: - getUserByUsername: changed return type to LiveData - getUserByID: changed return type to LiveData - getAllUsersLiveData: changed return type to LiveData - Added insertUser - Added removeUser - Added getRepository

--- app/src/main/java/com/example/ghettoyelp/Database/UserRepository.java
+++ app/src/main/java/com/example/ghettoyelp/Database/UserRepository.java
@@ -3,17 +3,20 @@ package com.example.ghettoyelp.Database;
 import android.app.Application;
 
 
+import androidx.lifecycle.LiveData;
+
 import com.example.ghettoyelp.Database.DAOs.UserDAO;
 import com.example.ghettoyelp.Database.Entities.User;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 /**
  * @author Yui Nguyen
- * Last Update: Dec 4th, 2024
+ * Last Update: Dec 5th, 2024
  * Description:
  *      A repository to read and write data to UserDAO
  */
@@ -47,21 +50,8 @@ public class UserRepository {
 
     // METHODS to READ and WRITE to DAO
     // Get all users
-    public ArrayList<User> getAllUsers(){
-        Future<ArrayList<User>> future = MainDatabase.databaseExecutor.submit(
-                new Callable<ArrayList<User>>() {
-                    @Override
-                    public ArrayList<User> call() throws Exception {
-                        return (ArrayList<User>) userDAO.getAllUsers();
-                    }
-                }
-        );
-        try{
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            //Log.i(MainActivity.TAG, "Problem in the repository - getAllLogs");
-        }
-        return null;
+    public LiveData<List<User>> getAllUsersLiveData(){
+        return userDAO.getAllUsers();
     }
 
     public static UserRepository getRepository(Application application){
@@ -79,44 +69,18 @@ public class UserRepository {
         try{
             return future.get();
         }  catch (InterruptedException | ExecutionException e) {
-            //Log.i(MainActivity.TAG, "Problem in the GymLogRepository");
+            //Log.i(MainActivity.TAG, "Problem in the ...");
         }
         return null;
     }
 
 
-    public User getUserByUsername(String username){
-        Future<User> future = MainDatabase.databaseExecutor.submit(
-                new Callable<User>() {
-                    @Override
-                    public User call() throws Exception {
-                        return userDAO.getUserByUsername(username);
-                    }
-                }
-        );
-        try{
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            //Log.i(MainActivity.TAG, "Problem in the repository - getAllLogs");
-        }
-        return null;
+    public LiveData<User> getUserByUsername(String username){
+        return userDAO.getUserByUsername(username);
     }
 
-    public User getUserByID(int ID){
-        Future<User> future = MainDatabase.databaseExecutor.submit(
-                new Callable<User>() {
-                    @Override
-                    public User call() throws Exception {
-                        return userDAO.getUserByID(ID);
-                    }
-                }
-        );
-        try{
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            //Log.i(MainActivity.TAG, "Problem in the repository - getAllLogs");
-        }
-        return null;
+    public LiveData<User> getUserByID(int userID){
+        return userDAO.getUserByID(userID);
     }
 
     // TODO: wait for other issues to add other methods
