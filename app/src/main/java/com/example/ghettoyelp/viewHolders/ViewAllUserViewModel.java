4cbdc20ebc05522f0103faa07d0adfecb0fd2333
JCline11 <jasonsmevog@gmail.com>
2024-12-04 18:55:03 -0800

    Added Title to activity_view_all_users.xml - Created and worked on ViewAllUserViewModel - NEED functions in UserRepository

---
+++ app/src/main/java/com/example/ghettoyelp/viewHolders/ViewAllUserViewModel.java
@@ -0,0 +1,31 @@
+package com.example.ghettoyelp.viewHolders;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.ghettoyelp.Database.Entities.User;
+import com.example.ghettoyelp.Database.UserRepository;
+
+import java.util.List;
+
+public class ViewAllUserViewModel extends AndroidViewModel {
+    private UserRepository repository;
+
+    private final LiveData<List<User>> allUsers;
+
+    public ViewAllUserViewModel (Application application) {
+        super(application);
+        repository = UserRepository.getRepository(application);
+        allUsers = repository.getAllUsersLiveData();
+    }
+
+    public LiveData<List<User>> getAllUsers(){
+        return allUsers;
+    }
+
+    public void insUser(User user){
+        repository.insertUser(user);
+    }
+}
