3d5b6bfc0b9d8652ce8359a1a1581d8ea6136259
Nguyen <yunguyen@csumb.edu>
2024-12-05 11:33:33 -0800

    - Changed return type to LiveData in ReviewDAO and UserDAO

--- app/src/main/java/com/example/ghettoyelp/Database/DAOs/UserDAO.java
+++ app/src/main/java/com/example/ghettoyelp/Database/DAOs/UserDAO.java
@@ -1,5 +1,6 @@
 package com.example.ghettoyelp.Database.DAOs;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -14,14 +15,13 @@ import java.util.List;
 
 /**
  * @author Yui Nguyen
- * Last Update: Dec 4th, 2024
+ * Last Update: Dec 5th, 2024
  * Description:
  *      Data Access Object for Entity Class - User
  */
 
 @Dao
 public interface UserDAO {
-    // TODO: wait for User entity class to be created
     // Method to add a new user into database table
     // If there is any conflict, replace with a new one
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -29,13 +29,13 @@ public interface UserDAO {
 
     // METHODS to get data from USER_TABLE
     @Query("SELECT * FROM " + MainDatabase.USER_TABLE + " ORDER BY username DESC")
-    List<User> getAllUsers();
+    LiveData<List<User>> getAllUsers();
 
     @Query("SELECT * FROM " + MainDatabase.USER_TABLE + " WHERE username == :name")
-    User getUserByUsername(String name);
+    LiveData<User> getUserByUsername(String name);
 
     @Query("SELECT * FROM " + MainDatabase.USER_TABLE + " WHERE id == :ID")
-    User getUserByID(int ID);
+    LiveData<User> getUserByID(int ID);
 
     // METHODS to delete user
     @Delete
