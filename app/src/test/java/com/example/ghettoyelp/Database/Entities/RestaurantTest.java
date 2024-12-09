c59f4f9bc83694b04bd3a8edb5bfbad2a9084784
Nguyen <yunguyen@csumb.edu>
2024-12-09 09:31:51 -0800

    Created Test files for Entities - Restaurant, Review, User

---
+++ app/src/test/java/com/example/ghettoyelp/Database/Entities/RestaurantTest.java
@@ -0,0 +1,28 @@
+package com.example.ghettoyelp.Database.Entities;
+
+import junit.framework.TestCase;
+
+public class RestaurantTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testSetId() {
+    }
+
+    public void testTestSetName() {
+    }
+
+    public void testSetRating() {
+    }
+
+    public void testSetTotalReviews() {
+    }
+
+    public void testSetDescription() {
+    }
+}
---
+++ app/src/test/java/com/example/ghettoyelp/Database/Entities/UserTest.java
@@ -0,0 +1,28 @@
+package com.example.ghettoyelp.Database.Entities;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testSetId() {
+    }
+
+    public void testSetUsername() {
+    }
+
+    public void testSetPassword() {
+    }
+
+    public void testSetReviewsCount() {
+    }
+
+    public void testIsAdmin() {
+    }
+}
