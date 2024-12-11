36c6d9f46e67cef1e6d6ae3ccb4e7cca19dd824e
JCline11 <jasonsmevog@gmail.com>
2024-12-10 19:47:43 -0800

    Finished ManageReviewViewHolder

--- app/src/main/java/com/example/ghettoyelp/viewHolders/ViewAllUsers/ManageReviews/ManageReviewViewHolder.java
+++ app/src/main/java/com/example/ghettoyelp/viewHolders/ViewAllUsers/ManageReviews/ManageReviewViewHolder.java
@@ -1,4 +1,28 @@
 package com.example.ghettoyelp.viewHolders.ViewAllUsers.ManageReviews;
 
-public class ManageReviewViewHolder {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ghettoyelp.R;
+
+public class ManageReviewViewHolder extends RecyclerView.ViewHolder {
+    TextView reviewViewItem;
+    private ManageReviewViewHolder(View reviewView){
+        super(reviewView);
+        reviewViewItem = reviewView.findViewById(R.id.reviewsRecyclerItemTextView);
+    }
+
+    public void bind (String text){
+        reviewViewItem.setText(text);
+    }
+
+    static ManageReviewViewHolder create (ViewGroup parent){
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.manage_review_recycler, parent, false);
+        return new ManageReviewViewHolder(view);
+    }
 }
