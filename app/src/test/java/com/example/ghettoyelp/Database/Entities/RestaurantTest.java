4d470351de43eeb59ecce1deba845e681f7eff5f
yusraashar <yashar@csumb.edu>
2024-12-12 14:07:36 -0800

    public class RestaurantTest extends TestCase {

--- app/src/test/java/com/example/ghettoyelp/Database/Entities/RestaurantTest.java
+++ app/src/test/java/com/example/ghettoyelp/Database/Entities/RestaurantTest.java
@@ -1,28 +1,75 @@
 package com.example.ghettoyelp.Database.Entities;
 
+import static org.junit.Assert.assertEquals;
+
 import junit.framework.TestCase;
 
-public class RestaurantTest extends TestCase {
+import org.junit.Before;
+import org.junit.Test;
 
-    public void setUp() throws Exception {
-        super.setUp();
+/**
+ * @author yusraashar
+ * Unit tests for the Restaurant entity class.
+ */
+public class RestaurantTest  {
+
+   private Restaurant restaurant;
+
+    /**
+     * Set up a new Restaurant object before each test.
+     */
+    @Before
+    public void setUp() {
+        restaurant = new Restaurant();
     }
 
-    public void tearDown() throws Exception {
-    }
-
+    /**
+     * Test setting and getting the ID of a restaurant.
+     */
+    @Test
     public void testSetId() {
+        restaurant.setId(1);
+        assertEquals(1, restaurant.getId());
     }
 
-    public void testTestSetName() {
+    /**
+     * Test setting and getting the name of a restaurant.
+     */
+    @Test
+    public void testSetName() {
+        String name = "Test Restaurant";
+        restaurant.setName(name);
+        assertEquals(name, restaurant.getName());
     }
 
+    /**
+     * Test setting and getting the rating of a restaurant.
+     */
+    @Test
     public void testSetRating() {
+        double rating = 4.5;
+        restaurant.setRating(rating);
+        assertEquals(rating, restaurant.getRating(), 0.0);
     }
 
-    public void testSetTotalReviews() {
-    }
-
+    /**
+     * Test setting and getting the description of a restaurant.
+     */
+    @Test
     public void testSetDescription() {
+        String description = "A cozy place to dine.";
+        restaurant.setDescription(description);
+        assertEquals(description, restaurant.getDescription());
+    }
+
+    /**
+     * Test the default values of a new Restaurant object.
+     */
+    @Test
+    public void testDefaultValues() {
+        assertEquals(0, restaurant.getId());
+        assertEquals(null, restaurant.getName());
+        assertEquals(null, restaurant.getDescription());
+        assertEquals(0.0, restaurant.getRating(), 0.0);
     }
 }
